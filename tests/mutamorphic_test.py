aaf8c0c82ba10a4a3f63c7f682ff5c390454c30c
Maarten van Tartwijk <m.j.vantartwijk@student.tudelft.nl>
2022-06-20 13:48:40 +0200

    Rename files.

--- tests/mutamorphic_test.py
+++ tests/mutamorphic_test.py
@@ -1,85 +1,114 @@
-from mutatest.test_runner import MutamorphicTest, MutamorphicTestCase
-from mutatest.mutators import ReplacementMutator, DropoutMutator
-from src.p1_preprocessing import text_prepare
-from src.p2_text_processors import bag_of_words
-from sklearn.multiclass import OneVsRestClassifier
-from sklearn.metrics import mean_squared_error
-from joblib import dump, load
+from joblib import load
+import json
+import random
+import sys
+import os
 import numpy as np
-import pandas as pd
-from os import path
-from random import Random
-from typing import List
 
-MODEL_FOLDER = "../output/"
-DATA_FOLDER = "../data/"
-WORDS_TO_INDEX = load(path.join(MODEL_FOLDER, 'words_to_index.joblib'))
-DICT_SIZE = len(WORDS_TO_INDEX)
-CLASSIFIERS = load(path.join(MODEL_FOLDER, 'classifiers.joblib'))
-TEST_DATA = pd.read_csv(path.join(DATA_FOLDER, 'test.tsv'), sep='\t', dtype={"title": object})[["title"]].values.tolist()
-TEST_DATA = [x[0] for x in TEST_DATA]
+# sys.path.append(os.getcwd())
 
 SEED = 600
-NUMBER_OF_TEST_CASES = 20000
-MAXIMUM_DISTANCE = 20.0
+random.seed(SEED)
+LIMIT = 2000
 
 
-def get_input_sentences(rng: Random) -> List[str]:
-    return rng.sample(TEST_DATA, NUMBER_OF_TEST_CASES)
+def load_test_phrases(limit=LIMIT):
+
+    from src import p1_preprocessing
+
+    validation_data = p1_preprocessing.read_data("data/validation.tsv")
+
+    X_val, y_val = validation_data['title'].values, validation_data['tags'].values
+
+    lower_limit = random.randint(0, len(X_val)-limit-1)
+    upper_limit = lower_limit+limit
+
+    return X_val[lower_limit:upper_limit], y_val[lower_limit:upper_limit]
 
 
-def run_bag_of_words(sentence: str) -> np.ndarray:
-    classifier: OneVsRestClassifier = CLASSIFIERS['bag']
-    cleaned_input = text_prepare(sentence)
-    input_vector = bag_of_words(cleaned_input, WORDS_TO_INDEX, DICT_SIZE)
-    input_vector = np.expand_dims(input_vector, 0)
+def load_x_train():
+    from src import p1_preprocessing
 
-    scores = classifier.decision_function(input_vector)
-
-    return scores
+    return p1_preprocessing.read_data(f'data/train.tsv')['title'].values
 
 
-def distance_function(vector_a: np.ndarray, vector_b: np.ndarray) -> float:
-    return mean_squared_error(vector_a, vector_b)
+def change_phrases(X_val, num_replacements=2, num_variants=3, selection_strategy="random"):
+    from mutatest.mutators import ReplacementMutator
+
+    mutator = ReplacementMutator(num_replacements, num_variants, selection_strategy)
+
+    new_x_vals = []
+    for i in range(0, num_variants):
+        new_x_vals.append(list())
+
+    for sentence in X_val:
+        results = mutator.mutate(sentence, SEED)
+
+        for i in range(0, num_variants):
+
+            if len(results) == num_variants:
+                new_x_vals[i].append(results[i])
+            else:
+                # This way we ENSURE that both results have equal length
+                new_x_vals[i].append(sentence)
+
+    return new_x_vals
+    # To be implemented
 
 
-def test_mutamorphic_bow_replacement():
-    rng = Random()
-    rng.seed(SEED)
-    input_sentences = get_input_sentences(rng)
-    test: MutamorphicTest[np.ndarray] = MutamorphicTest(
-        input_sentences,
-        ReplacementMutator(num_replacements=1, num_variants=5),
-        run_bag_of_words,
-        distance_function,
-        random_seed=SEED
-    )
-    test.run()
+def get_values_classifiers(X_val, y_val, X_train):
+    from src import p1_preprocessing
+    from src import p2_text_processors
+    X_val = [p1_preprocessing.text_prepare(x) for x in X_val]
 
-    dump(test, "test_object_replacement50.joblib")
+    DICT_SIZE, INDEX_TO_WORDS = p2_text_processors.get_tags_and_words(X_val, y_val)[:2]
 
-    assert test.average_similarity < MAXIMUM_DISTANCE
+    tfidf_vectorizer = p2_text_processors.TfidfVectorizer(min_df=5, max_df=0.9, ngram_range=(1, 2),
+                                                          token_pattern='(\S+)')
 
+    X_train = [p1_preprocessing.text_prepare(x) for x in X_train]
 
-def test_mutamorphic_bow_dropout():
-    rng = Random()
-    rng.seed(SEED)
-    input_sentences = get_input_sentences(rng)
-    test: MutamorphicTest[np.ndarray] = MutamorphicTest(
-        input_sentences,
-        DropoutMutator(num_dropouts=1, num_variants=5),
-        run_bag_of_words,
-        distance_function,
-        random_seed=SEED
-    )
-    test.run()
+    X_val_bag = p2_text_processors.get_x_values_bag(INDEX_TO_WORDS, DICT_SIZE, X_val)
+    tfidf_vectorizer.fit_transform(X_train)
 
-    dump(test, "test_object_dropout.joblib")
+    X_val_tfidf = tfidf_vectorizer.transform(X_val)
 
-    assert test.average_similarity < MAXIMUM_DISTANCE
+    return X_val_bag, X_val_tfidf
 
 
-if __name__ == "__main__":
+def test_mutamorphic():
 
-    test_mutamorphic_bow_replacement()
-    test_mutamorphic_bow_dropout()
+    # from src import p2_text_processors
+    from dependencies.model_functions import check_diff, get_diff_stats
+
+    X_val, y_val = load_test_phrases()
+    X_train = load_x_train()
+
+    classifiers = load("tests/dependencies/classifiers.joblib")
+    y_val = load('tests/dependencies/val_data.joblib')[:LIMIT]
+    bag_classifier = classifiers["bag"]
+    tfidf_classifier = classifiers["tfidf"]
+
+    # First we ge the unaltered labels
+
+    X_val_bag, X_val_tfidf = get_values_classifiers(X_val, y_val, X_train)
+
+    labels_bag_og = bag_classifier.predict(X_val_bag)
+    labels_tfidf_og = tfidf_classifier.predict(X_val_tfidf)
+
+    # Now we go for the mutants
+    X_val_mutates = change_phrases(X_val)
+
+    for X_val_mutate in X_val_mutates:
+        X_val_bag, X_val_tfidf = get_values_classifiers(np.array(X_val_mutate), y_val, X_train)
+
+        labels_bag = bag_classifier.predict(X_val_bag)
+        labels_tfidf = tfidf_classifier.predict(X_val_tfidf)
+
+        check_diff(get_diff_stats(labels_bag_og, labels_bag, y_val))
+        check_diff(get_diff_stats(labels_tfidf_og, labels_tfidf, y_val))
+
+    print("All tests passed.")
+
+
+test_mutamorphic()
